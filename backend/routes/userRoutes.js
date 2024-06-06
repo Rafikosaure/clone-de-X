e1fa7c9dee12270b64619c78e04f9d00f412ab42
RAFIK BEN_SADI <RAFIKBENSADI@live.fr>
2024-06-06 15:47:56 +0200

    Mise en place de la route et du controlleur de création de compte utilisateur.

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,9 @@
+import express from 'express'
+import userCtrl from '../controllers/userControllers.js'
+const router = express.Router()
+
+router.post('/register', userCtrl.register)
+// router.post('/login', userCtrl.login)
+
+
+export default router
